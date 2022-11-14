7f4b7353eb5d3a4c40eac5d778ebdd24d8776eeb
Dennis <dennis.piecha25@gmail.com>
2022-11-14 13:27:43 +0100

    home

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React, { useContext } from "react";
 
 export default function Home() {
-  
   return (
-  <div>
-    Home
-  </div>
-  )
+    <div className="Home">
+      <div>
+        <h1>Veranstaltungen in deiner Nähe</h1>
+        <p>Wo bist du gerade?</p>
+        <input type="text" />
+        <button>Los geht´s!</button>
+      </div>
+      <div>
+        <h2>Veranstaltung erstellen</h2>
+        <button>Erstellen</button>
+      </div>
+      Home
+    </div>
+  );
 }
